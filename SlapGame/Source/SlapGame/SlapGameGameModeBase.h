42cec2756d7f3c4cb256d007c262baed670bf8d7
irene.amor <irene.amor@digipen.edu>
2023-09-29 12:20:31 +0200

    New project, SLAP GAME

---
+++ SlapGame/Source/SlapGame/SlapGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SlapGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLAPGAME_API ASlapGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
